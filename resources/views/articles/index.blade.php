7ce31163024c6b5daba9ad71faf475690a32c748
Richard Leitch <rak_leitch@hotmail.com>
2024-09-19 12:26:19 +0100

    Basic CRUD

---
+++ resources/views/articles/index.blade.php
@@ -0,0 +1,16 @@
+<x-public-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('All Articles') }}
+        </h2>
+    </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <x-articles-table :articles="$articles" show-actions="0"></x-articles-table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-public-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,20 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('dashboard', [DashboardController::class, 'dashboard'])->middleware(['verified'])->name('dashboard');
+    Route::get('profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('articles', ArticleController::class);
 
 require __DIR__.'/auth.php';
